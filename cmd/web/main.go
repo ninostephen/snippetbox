18d92cc37c2f144efe63297665e879989478c3a0
Nino Stephen <ninostephen.dev@gmail.com>
2023-10-10 10:10:38 +0530

    Let's GOOOOOOOOOO!!!!!!!!!!!

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -7,13 +7,16 @@ import (
 
 func main() {
 	mux := http.NewServeMux()
+
+	fileServer := http.FileServer(http.Dir("./ui/static/"))
+	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
+
 	mux.HandleFunc("/", home)
 	mux.HandleFunc("/snippet/view", snippetView)
 	mux.HandleFunc("/snippet/create", snippetCreate)
 
 	log.Println("Starting server on port 8000")
 	err := http.ListenAndServe(":8000", mux)
-	if err != nil {
-		log.Fatal(err)
-	}
+	log.Fatal(err)
+
 }

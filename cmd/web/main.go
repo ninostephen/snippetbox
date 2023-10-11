bbdf66e3d25f96f467452bf94129e88bb27b0305
Nino Stephen <ninostephen.dev@gmail.com>
2023-10-11 08:26:05 +0530

    First attempt att adding CLI using flag module.

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,11 +1,16 @@
+// Package main is the entry point into our application.
 package main
 
 import (
+	"flag"
 	"log"
 	"net/http"
 )
 
 func main() {
+	addr := flag.String("addr", ":8000", "HTTP Network Address")
+	flag.Parse()
+
 	mux := http.NewServeMux()
 
 	fileServer := http.FileServer(http.Dir("./ui/static/"))
@@ -15,8 +20,8 @@ func main() {
 	mux.HandleFunc("/snippet/view", snippetView)
 	mux.HandleFunc("/snippet/create", snippetCreate)
 
-	log.Println("Starting server on port 8000")
-	err := http.ListenAndServe(":8000", mux)
+	log.Println("Starting server on port", *addr)
+	err := http.ListenAndServe(*addr, mux)
 	log.Fatal(err)
 
 }

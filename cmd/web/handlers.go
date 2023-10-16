067590fe90207df9a77960b7df142cd69f18ce65
Nino Stephen <ninostephen.dev@gmail.com>
2023-10-16 09:38:03 +0530

    Some major progress with DB

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -44,5 +44,13 @@ func (app *application) snippetCreate(w http.ResponseWriter, r *http.Request) {
 		app.clientError(w, http.StatusMethodNotAllowed)
 		return
 	}
-	w.Write([]byte("New snippet created"))
+	title := "0 snail"
+	content := "O snail\nClimb Mount Fuji,\nBut slowly, slowly!\n\n– Kobayashi Issa"
+	expires := 7
+	id, err := app.snippets.Insert(title, content, expires)
+	if err != nil {
+		app.serverError(w, err)
+	}
+	http.Redirect(w, r, fmt.Sprintf("/snippet/view?id=%d", id), http.StatusSeeOther)
+
 }
